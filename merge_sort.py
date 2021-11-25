bd26ae2ee1a2d6f8d9769fdf0a568525a9e856ed
Janujan Sritharan <janujan.sritharan27@gmail.com>
2021-11-25 13:26:35 -0500

    Forgot to format

--- merge_sort.py
+++ merge_sort.py
@@ -5,15 +5,16 @@ def merge(arr1, arr2):
     while p1 < len(arr1) and p2 < len(arr2):
         if arr1[p1] > arr2[p2]:
             new.append(arr2[p2])
-            p2+=1
+            p2 += 1
         else:
             new.append(arr1[p1])
-            p1+=1
+            p1 += 1
     if p1 == len(arr1):
         new.extend(arr2[p2:])
     else:
         new.extend(arr1[p1:])
     return new
+
 
 def merge_sort(arr):
     if len(arr) == 1:
@@ -25,4 +26,3 @@ def merge_sort(arr):
             return [arr[1], arr[0]]
     split = len(arr) // 2
     return merge(merge_sort(arr[:split]), merge_sort(arr[split:]))
-
