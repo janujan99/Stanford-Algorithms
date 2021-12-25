4999965e5ebc1aba508c668483b0edb10aa0ad27
Janujan Sritharan <janujan.sritharan27@gmail.com>
2021-12-24 21:47:27 -0500

    More functional min cut

--- karger_min_cut.py
+++ karger_min_cut.py
@@ -13,29 +13,46 @@ def min_cut(graph, contraction_lst):
             b = key_list[random.randint(0, len(graph)-1)]
             if a != b and a in graph[b]:
                 valid_edge = True
-        # whichever node has label with smaller value, keep that one during the contraction and discard node with higher label value later
+
+        # add edge to be contracted to contraction_lst
         contraction_lst.append((a, b))
+
+        # whichever node has label with smaller value, keep that one during the contraction and discard node with higher label value later
         kept_vertex = min([a, b])
         removed_vertex = max([a, b])
 
         # add all edges from removed_vertex's edge list to kept_vertex's edge list, with the exception of the removed vertex (avoid adding self loops)
-        arr = [x for x in graph[kept_vertex] if x != removed_vertex]
+        arr = [x for x in graph[kept_vertex] if x != kept_vertex]
         arr.extend([v for v in graph[removed_vertex] if v != kept_vertex])
 
         graph[kept_vertex] = [y for y in arr]
 
-        # remove the removed_vertex from all other edge lists
+        # replace the removed_vertex from all other edge lists with kept_vertex, unless the iteration is on the kept_vertex itself
         for node in graph:
-            graph[node] = [x for x in graph[node] if x != removed_vertex]
-
+            if node == kept_vertex:
+                graph[node] = [x for x in graph[node] if x != removed_vertex]
+            else:
+                arr = []
+                for x in graph[node]:
+                    if x == removed_vertex:
+                        arr.append(kept_vertex)
+                    else:
+                        arr.append(x)
+                graph[node] = arr
         # remove the removed_vertex from graph
         del graph[removed_vertex]
+    print(len(graph[list(graph.keys())[0]]) == len(graph[list(graph.keys())[1]]))
+    return len(graph[list(graph.keys())[0]])
 
-    return max([len(graph[list(graph.keys())[0]]), len(graph[list(graph.keys())[1]])])
+graph = {}
+lines = open("kargerMinCut.txt", "r").readlines()
+
+for line in lines:
+    split = [int(x) for x in line.split("\t")[:-1]]
+    graph[split[0]] = split[1:]
 
 
-contr_list = []
-adj_list = {1: [2, 3, 4], 2: [1, 4], 3: [1, 4, 5], 4: [1, 2, 3], 5: [3]}
-print("Min cut: " + str(min_cut(adj_list, contr_list)))
-print("Graph: " + str(adj_list))
-print("Cut order: " + str(contr_list))
+for x in range(100000):
+    print("Min cut: " + str(min_cut(graph, [])))
+#print("Graph: " + str(graph))
+#print("Cut order: " + str(contr_list))
