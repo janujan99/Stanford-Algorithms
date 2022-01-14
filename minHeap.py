8a2c93c184a926fae673bb7c298f2d3ec7a9b5f4
Janujan Sritharan <janujan.sritharan27@gmail.com>
2022-01-13 23:16:15 -0500

    Median maintenance, fixed bug in heaps

--- minHeap.py
+++ minHeap.py
@@ -1,18 +1,21 @@
 class MinHeap:
     def __init__(self, lst=[]) -> None:
-        self.lst = lst
-        self.length = len(lst)
+        self.lst = []
+        for num in lst:
+            self.add(num)
+        
 
     def getParentIndex(self, index):
-        return max(0, (index-2)//2)
+        assert index > 0
+        return (index-1)//2
 
     def getLeftChildIndex(self, index):
-        if index*2 + 1 >= self.length:
+        if index*2 + 1 >= len(self.lst):
             return None
         return index*2 + 1
 
     def getRightChildIndex(self, index):
-        if index*2 + 2 >= self.length:
+        if index*2 + 2 >= len(self.lst):
             return None
         return index*2 + 2
 
@@ -23,47 +26,72 @@ class MinHeap:
         return
 
     def heapifyUp(self):
-        index = self.length - 1
+        index = len(self.lst) - 1
         while (index > 0 and self.lst[self.getParentIndex(index)] > self.lst[index]):
             self.swap(self.getParentIndex(index), index)
             index = self.getParentIndex(index)
         return
 
-    def heapifyDown(self):
-        index = 0
+    def heapifyDown(self, root):
+        left = self.getLeftChildIndex(root)
+        right = self.getRightChildIndex(root)
 
-        while self.getLeftChildIndex(index):
-            new_index = self.getLeftChildIndex(index)
-            if self.getRightChildIndex(index) and self.lst[self.getRightChildIndex(index)] < self.lst[new_index]:
-                new_index = self.getRightChildIndex(index)
-            if self.lst[index] > self.lst[new_index]:
-                self.swap(index, new_index)
-                index = new_index
+        if left is None:
+            return
+        
+        if right is None:
+            if self.lst[root] > self.lst[left]:
+                self.swap(root, left)
+                self.heapifyDown(left) 
             else:
-                break
-        return
+                return
+        
+        else:
+            lesser_element_index = left if self.lst[left] < self.lst[right] else right
+            if self.lst[root] < self.lst[lesser_element_index]:
+                return  
+            else:
+                self.swap(root, lesser_element_index)
+                self.heapifyDown(lesser_element_index)
+        return 
+        
+
+
 
     def add(self, item):
         self.lst.append(item)
-        self.length += 1
         self.heapifyUp()
         return
 
     def extract_min(self):
-        if self.length == 0:
+        if len(self.lst) == 0:
             return None
         minimum = self.lst[0]
-        self.lst[0] = self.lst[self.length-1]
+        self.lst[0] = self.lst[len(self.lst)-1]
         self.lst = self.lst[:-1]
-        self.length += -1
-        self.heapifyDown()
+        self.heapifyDown(0)
         return minimum
 
+    def peek_min(self):
+        if len(self.lst) == 0:
+            return None
+        return self.lst[0]
 
-heap = MinHeap([3, 4, 8, 9, 7, 10, 9, 15, 20, 13])
+    def get_length(self):
+        return len(self.lst)
 
-while heap.length > 0:
-    mine = heap.extract_min()
-    print("Minimum: " + str(mine))
-    print(heap.lst)
-    sc = input()
+import random
+
+nums = [random.randint(0,100000) for x in range(100)]
+
+heap = MinHeap(nums)
+
+length = heap.get_length()
+
+result = [heap.extract_min() for x in range(length)]
+expected = sorted(nums)
+
+for i in range(len(result)):
+    print(result[i], expected[i])
+    assert result[i] == expected[i]
+
